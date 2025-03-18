d41273091196b0d609264cc4e6d29ed13c272f58
PES1UG22CS529 <151939646+sanjana-v-patil@users.noreply.github.com>
2025-03-18 15:58:47 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  count<<"Sanjana V Patil PES1UG22CS529 \n";
+  cout<<"Sanjana V Patil PES1UG22CS529 \n";
   return 0;
 }
